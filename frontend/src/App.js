35828b78bc172a514976a6dd8647b50b17325670
Momen Abdelwadoud <momenwadoudg@gmail.com>
2022-05-19 10:13:13 +0200

    Setup theme and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffd41d",
+      light: "#FFE600",
+      veryLight: "#FFF06B",
+      dark: "#F0CD38",
+      veryDark: "#B4A200",
+    },
+    secondary: {
+      main: "#252525",
+      light: "#606060",
+      black: "#000000",
+      white: "#ffffff",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App"></div>
+    </ThemeProvider>
   );
 }
 
