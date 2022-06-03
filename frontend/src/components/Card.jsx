92d352e3d611015b910bd891bb6a6810a40e647b
Momen Abdelwadoud <momenwadoudg@gmail.com>
2022-06-03 20:36:46 +0200

    Finished schedule page

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -4,27 +4,41 @@ import AvatarImage from './AvatarImage';
 import { Typography } from '@mui/material';
 import Profile from './Profile';
 
+import Modal from "@mui/material/Modal";
+
+
 const Card = ({data}) => {
     const [open, setOpen] = useState(false)
-    const handleOpen = () =>{
-        setOpen(true)
-    }
+    const closeCardWindow = () => {
+        setOpen(false);
+    };
+
+    let card_data ={
+        name: data.name,
+        group: data.group,
+        attendance: data.attendance_percentage,
+        score: data.overall_score,
+      }
+    
+
     return (
         <>
         <div className={styles.card} onClick={handleOpen}>
             <AvatarImage className={styles.avatar}></AvatarImage>
-            <Typography fontFamily='roboto' component='h3' color='primary' variant={'title1'} letterSpacing={1.5}>{data.name.toUpperCase()}</Typography>
-            <Typography fontFamily='roboto' component='p' variant='caption' color='secondary.light'>Group {data.group}</Typography>
-            {/* <hr width='30%' style={{color:'#ffffff'}}/> */}
-            {Object.entries(data).forEach(([key,val])=>{
-                    return (<div>
-                <Typography fontFamily='roboto' component='p' variant='body2' color='secondary'>{key}</Typography>
-                <Typography fontFamily='roboto' component='p' variant='body2' fontWeight={'bold'} color='secondary'>{val}</Typography>
+            <Typography fontFamily='roboto' component='h3' color='primary' variant={'body1'} letterSpacing={1.2}>{card_data.name.toUpperCase()}</Typography>
+            <Typography fontFamily='roboto' component='p' variant='caption' color='secondary.light'>Group {card_data.group}</Typography>
+            <hr width='30%' style={{color:'#ffffff'}}/>
+            {Object.keys(card_data).map((key)=>{
+                return (<div className={styles.card_details}>
+                <Typography fontFamily='roboto' component='p' variant='caption' color='secondary'>{key}</Typography>
+                <Typography fontFamily='roboto' component='p' variant='caption' fontWeight={'bold'} color='secondary'>{card_data[key]}</Typography>
                     </div>
                 )
-                })}
-        </div>
-        {open ? <Profile ></Profile>: null}
+            })}
+            </div>
+            <Modal open={open} onClose={closeCardWindow}>
+            <Profile data={data}></Profile>
+        </Modal>
         </>
     );
 }
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,6 +1,5 @@
 import React,{useState} from 'react'
 import Typography from "@mui/material/Typography";
-import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import AvatarImage from './AvatarImage';
 
@@ -8,34 +7,22 @@ import AvatarImage from './AvatarImage';
 import styles from '../styles/Profile.module.css'
 
 export default function Profile({data}) {
-    const [open, setOpen] = useState(false)
-    const closeAddWindow = () => {
-        setOpen(false);
-    };
+    
   
   return (
-        <Modal
-          open={open}
-          onClose={closeAddWindow}
-        >
-          <Box className={styles.modal}>
-
-            <AvatarImage className={styles.avatar}></AvatarImage>
-            <Typography fontFamily='roboto' component='h3' color='primary' variant={'title1'} letterSpacing={1.5}>{data.name.toUpperCase()}</Typography>
-            <Typography fontFamily='roboto' component='p' variant='caption' color='secondary.light'>Group {data.group}</Typography>
-            <hr width='30%' style={{color:'#ffffff'}}/>
-            <div className={styles.profile_details}>
-                {Object.entries(data).forEach(([key,val])=>{
-                    return (<div>
-                <Typography fontFamily='roboto' component='p' variant='body2' color='secondary'>{key}</Typography>
-                <Typography fontFamily='roboto' component='p' variant='body2' fontWeight={'bold'} color='secondary'>{val}</Typography>
+    <Box className={styles.modal} >
+        <AvatarImage className={styles.avatar}></AvatarImage>
+        <Typography fontFamily='roboto' component='h3' color='primary' variant={'body1'} letterSpacing={1.2}>{data.name.toUpperCase()}</Typography>
+        <Typography fontFamily='roboto' component='p' variant='caption' color='secondary.light'>Group {data.group}</Typography>
+        <hr width='30%' style={{color:'#ffffff'}}/>
+        {Object.keys(data).map((key)=>{
+                return (<div className={styles.card_details}>
+                <Typography fontFamily='roboto' component='p' variant='caption' color='secondary'>{key}</Typography>
+                <Typography fontFamily='roboto' component='p' variant='caption' fontWeight={'bold'} color='secondary'>{data[key]}</Typography>
                     </div>
                 )
-                })}
-            </div>
-
-            
-          </Box>
-          </Modal>
+            })}
+        </Box>
+    
   )
 }
